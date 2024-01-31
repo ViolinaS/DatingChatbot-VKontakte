a5dcc0055fa0745bbad260b0130ae54cc0aaaeea
DimaT000 <157667845+DimaT000@users.noreply.github.com>
2024-01-31 12:20:09 +0300

    Update configdata.py

--- configdata.py
+++ configdata.py
@@ -1,5 +1,5 @@
-setting = {"group_token": "",
-           "group_id": ""}
+setting = {"group_token": "vk1.a.i8QL8yFW00L-8Lb159mYd8zrwqqhAN7nomV-F9E4yiWk6beAH0fRKvfShQXNky9EYsnQlhpoPz-q3OJ7E4dfK8gcjnGcCC7fLjt5nN_OY7x3IwsTbMOii8heqBFHo1FNPgveKm6xfQtFSF_Kmr97LJDP01rvLSElN35JeXWJokyI0u_TXnwr74Sp3NaYdA9q2QX3bJ6gFOkhR-tTfmosiQ",
+           "group_id": "224437988"}
 
-access_token = 
+access_token = 755800052
 postgres_password = 
